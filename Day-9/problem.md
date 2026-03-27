829ad28550f40635539e3b15c2e8d90858339d8f
OM AHIRE <omahire2601@gmail.com>
2026-03-27 18:37:56 +0530

    Day 9 updates
    Day 9 updates

---
+++ Day-9/problem.md
@@ -0,0 +1,1 @@
+Given a base- integer, , convert it to binary (base-). Then find and print the base- integer denoting the maximum number of consecutive 's in 's binary representation. When working with different bases, it is common to show the base as a subscript.
---
+++ Day-9/solution.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+binary = bin(n)[2:]
+
+max_count = 0
+current_count = 0
+
+for bit in binary:
+    if bit == '1':
+        current_count += 1
+        max_count = max(max_count, current_count)
+    else:
+        current_count = 0
+
+print(max_count)
