931619ae3f5f995b2ba90f32d6f1ef5c42f54b05
OM AHIRE <omahire2601@gmail.com>
2026-03-30 01:14:23 +0530

    "Deleted code for Day 9 and Day 6 problems"

---
+++ Day-11/problem.md
@@ -0,0 +1,17 @@
+Given a  2D Array, :
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+We define an hourglass in  to be a subset of values with indices falling in this pattern in 's graphical representation:
+
+a b c
+  d
+e f g
+There are  hourglasses in , and an hourglass sum is the sum of an hourglass' values.
+
+Task
+Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum.
---
+++ Day-11/solution.py
@@ -0,0 +1,21 @@
+if __name__ == '__main__':
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    max_sum = -float('inf')
+
+    for i in range(4):
+        for j in range(4):
+
+            current_sum = (
+                arr[i][j] + arr[i][j+1] + arr[i][j+2]
+                + arr[i+1][j+1] +
+                arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+            )
+
+            max_sum = max(max_sum, current_sum)
+
+    print(max_sum)
